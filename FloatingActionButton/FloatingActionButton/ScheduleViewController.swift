ae9541a41d6c94f232fe4fe20b7425841ad7484f
JinUng <ingjwjw@naver.com>
2022-08-13 19:48:38 +0900

    datepicker 구현

--- FloatingActionButton/FloatingActionButton/ScheduleViewController.swift
+++ FloatingActionButton/FloatingActionButton/ScheduleViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ScheduleViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UITextField!
+    
+    @IBOutlet weak var startDateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
-    
-
-    
+        
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyy-MM-dd HH:mm EEE"
+        startDateLabel.text = formatter.string(from: datePickerView.date)
+    }
 }
